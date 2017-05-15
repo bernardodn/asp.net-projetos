5f66321d157c90d92b5e26274229398b8e90b787
Bernardo Dolata Neto <dolataneto@gmail.com>
2017-05-15 11:24:51 -0300

    Criando formulário de projeto.

---
+++ Aula1505/Aula1505/CadastroProjeto.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Aula1505.Models;
+
+namespace Aula1505
+{
+    public partial class CadastroProjeto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Text = string.Empty;
+            txtDescricao.Text = string.Empty;
+            chkAtivo.Checked = false;
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Projeto prj = new Projeto();
+            prj.Nome = txtNome.Text;
+            prj.Descricao = txtDescricao.Text;
+            prj.Ativo = chkAtivo.Checked;
+            if (prj.Nome != null)
+            {
+                LimparCampos();
+            }
+        }
+    }
+}
