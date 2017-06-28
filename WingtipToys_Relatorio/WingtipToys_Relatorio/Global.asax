eefcb7638661f288425ec93cb8d09e1166ffd942
Bernardo Dolata Neto <dolataneto@gmail.com>
2017-06-28 09:32:34 -0300

    Adicionando Relatorio;

---
+++ WingtipToys_Relatorio/WingtipToys_Relatorio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WingtipToys_Relatorio.MvcApplication" Language="C#" %>
---
+++ WingtipToys_Relatorio/WingtipToys_Relatorio/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys_Relatorio.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+    }
+}
